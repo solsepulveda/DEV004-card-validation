d82200501a78fa9a935de16f449b038093c0ad00
solsepulveda <solangesepulved@gmail.com>
2023-01-31 10:56:31 -0300

    Código listo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import validator from './validator.js';
 
-console.log(validator);
+const boton = document.getElementById("validacion");
+boton.addEventListener("click", validate);
+const inputnumber=document.getElementById("cardnumber")
+function validate() {
+  const cardNumber = inputnumber.value;
+
+  if(cardNumber === ""){
+    alert ("El campo está vacío");
+  } else{
+    const isValid = validator.isValid(cardNumber);
+    const masked = validator.maskify(cardNumber);
+    if (isValid) {
+      document.getElementById("result").innerHTML = "Su tarjeta " + masked + " es válida";
+    }else{
+      document.getElementById("result").innerHTML = "Su tarjeta " + masked + " es inválida";
+    }
+  }
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,42 @@
 const validator = {
-  // ...
-};
+  isValid: function(creditCardNumber){
+    //*split lo devuelve en arreglo, por separado*//
+    const separar=creditCardNumber.split("").reverse();
+    //console.log(separar)
+    let total = 0;
+    for(let i = 0; i < separar.length; i++) {
+      let integro = parseInt(separar[i]);
+      //console.log()
+      if (i % 2 !== 0){
+        integro *= 2;
+        separar[i] = integro
+        //console.log(separar)
+        if (integro > 9){
+          integro -= 9;
+        }
+        separar[i] = integro
+      }
+      total += integro;
+      //console.log(total)
+    }
+    const verificacion = total % 10;
+    //console.log(verificacion)
+    //return verificacion;
+
+    if(verificacion  === 0){
+      return true;
+    }
+    else{
+      return false;
+    }
+  },
+  maskify(creditCardNumber) {
+    if (creditCardNumber.length < 4) return creditCardNumber;
+    const last4Characters = creditCardNumber.substr(-4);
+    const maskingCharacters = creditCardNumber.substr(0, creditCardNumber.length -4).replace(/./g, '#');
+    const masked = (maskingCharacters + last4Characters);
+    return masked;
+  }
+}
 
 export default validator;
